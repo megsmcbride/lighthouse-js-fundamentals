3184f372e5ac06067709571cc1ba5513b412f561
megan <meganambermcbride@gmail.com>
2022-05-19 12:27:56 -0400

    Add more conditional examples

---
+++ conditionals-whichSchool.js
@@ -0,0 +1,15 @@
+let whichSchool  = ''
+const age = 13
+
+  if (age <13) {
+    whichSchool = "Elementary School";
+  
+  } else if (age >= 13 || age < 18) {
+      whichSchool = "Secondary School";
+  
+  } else {
+    whichSchool = "Lighthouse Labs";
+  }
+
+console.log(whichSchool)
+
